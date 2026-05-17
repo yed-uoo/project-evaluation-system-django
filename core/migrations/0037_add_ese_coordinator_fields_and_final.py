cb5c8cf28e8ad47f3c8915c35aeb93d25a0754f0
yed-uoo <yedu11kr2005@gmail.com>
2026-05-17 12:59:14 +0530

    Fix duplicate ESE migration conflict

--- core/migrations/0037_add_ese_coordinator_fields_and_final.py
+++ core/migrations/0037_add_ese_coordinator_fields_and_final.py
@@ -1,5 +1,3 @@
-import django.core.validators
-
 from django.db import migrations, models
 
 class Migration(migrations.Migration):
@@ -16,16 +14,6 @@ class Migration(migrations.Migration):
 
             model_name='studentevaluation',
 
-            name='ese_coord1_submitted',
-
-            field=models.BooleanField(default=False),
-
-        ),
-
-        migrations.AddField(
-
-            model_name='studentevaluation',
-
             name='ese_coord1_submitted_at',
 
             field=models.DateTimeField(blank=True, null=True),
@@ -36,63 +24,9 @@ class Migration(migrations.Migration):
 
             model_name='studentevaluation',
 
-            name='ese_coord1_viva',
-
-            field=models.IntegerField(
-
-                default=0,
-
-                validators=[
-
-                    django.core.validators.MinValueValidator(0),
-
-                    django.core.validators.MaxValueValidator(25)
-
-                ]
-
-            ),
-
-        ),
-
-        migrations.AddField(
-
-            model_name='studentevaluation',
-
-            name='ese_coord2_submitted',
-
-            field=models.BooleanField(default=False),
-
-        ),
-
-        migrations.AddField(
-
-            model_name='studentevaluation',
-
             name='ese_coord2_submitted_at',
 
             field=models.DateTimeField(blank=True, null=True),
-
-        ),
-
-        migrations.AddField(
-
-            model_name='studentevaluation',
-
-            name='ese_coord2_viva',
-
-            field=models.IntegerField(
-
-                default=0,
-
-                validators=[
-
-                    django.core.validators.MinValueValidator(0),
-
-                    django.core.validators.MaxValueValidator(25)
-
-                ]
-
-            ),
 
         ),
 
