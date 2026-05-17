a6f28e14d5e8dec86a6e8ae6a3980d9ef6fdd932
yed-uoo <yedu11kr2005@gmail.com>
2026-05-17 12:33:55 +0530

    Fix migration dependency for postgres deploy

--- core/migrations/0014_remove_sustainabledevelopmentgoal_content_and_more.py
+++ core/migrations/0014_remove_sustainabledevelopmentgoal_content_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0013_fix_sdg_schema_compat'),
+    ('core', '0013_alter_abstract_id_alter_coordinatorapproval_id_and_more'),
     ]
 
     operations = [
